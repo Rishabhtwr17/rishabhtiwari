1a44897dedffe62c90eecf54ece4a6904b19a5ed
Rishabhtwr17 <rishabhtwr17@gmail.com>
2021-02-27 10:54:30 +0530

    CERTIFICATE UPDATES

---
+++ afterNumber.php
@@ -0,0 +1,14 @@
+<!DOCTYPE> 
+<html>
+<head>
+<meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8” />
+<title>Lesson 3</title>
+</head>
+<body>
+<?php
+echo ‘<h1>Lesson 3</h1>’;
+echo ‘<p>This is the first paragraph</p>’ ;
+echo ‘<p>This is the second paragraph</p>’;
+?>
+</body>
+</html>
